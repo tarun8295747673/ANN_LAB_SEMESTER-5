fbbdb8a8f579987fffb2b87f847d06b060b323ab
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-09-09 15:54:36 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,4 @@
+num1 = 10
+num2 = 20
+mul = num1*num2
+print("product of num1 and num2 is:",mul)
---
+++ prgram2.py
@@ -0,0 +1,14 @@
+name = "Tarun"
+age = 20
+height = 171 # in cm
+
+# display the ubove variable
+print("name:",name)
+print("age:",age)
+print("height",height,"in cm")
+print("name:",name,"age:",age,"height:",height)
+
+# using simple formating variable
+
+formatted_info = "name:{},age:{},height:{}".format(name,age,height)
+print(formatted_info)
